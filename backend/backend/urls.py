21219addcda9e04e680fc8060d36f0b56200d4fd
AngryMorrocoy <mrivasmahecha@gmail.com>
2022-02-14 23:51:07 -0400

    Serving static files so the admin panel has CSS

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"))]
+from backend.settings import STATIC_ROOT
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    re_path("static/(?P<path>).*", serve, {"document_root": STATIC_ROOT}),
+]
